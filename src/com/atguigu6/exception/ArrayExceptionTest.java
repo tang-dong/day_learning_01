ae412b0613dd40e14f1d4a2c97b9b7542dd5dd8f
tyd <tang3816697@foxmail.com>
2023-08-14 23:22:24 +0800

    第二天代码提交!

--- src/com/atguigu6/exception/ArrayExceptionTest.java
+++ src/com/atguigu6/exception/ArrayExceptionTest.java
@@ -9,11 +9,19 @@ package com.atguigu6.exception;
 public class ArrayExceptionTest {
 	public static void main(String[] args) {
 		// 1. 数组角标越界的异常：
-
+		int[] arr = new int[10];
+		//角标的有效范围：0、1、2、...、9
+		//System.out.println(arr[10]); //ArrayIndexOutOfBoundsException
 
 		// 2. 空指针异常：
+		int[] arr1 = new int[10];
+		arr1 = null;
 
+		//System.out.println(arr1[0]); //NullPointerException
 
+		int[][] arr2 = new int[3][];
+		arr2[0] = new int[10]; //此行代码不存在时，下一行代码出现NullPointerException
+		System.out.println(arr2[0][1]); //NullPointerException
 
 	}
 }
